7ebc437e00f78bd41876a60caca4ea6be10fdaeb
Nicole Sandry <nms377@nyu.edu>
2018-03-16 15:43:47 -1000

    Removed all the links and text I created previously. Will eventually move this to a container directory and import components of my work.

---
+++ client/src/components/Work.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Work extends Component {
+
+	render() {
+		return(
+			<div>
+				<h1>Work</h1>
+			</div>
+		)
+	}
+}
+
+export default Work;
