98c8e0fb5d07d67bf9dafcde39ec43379ea4add6
Nicole Sandry <nms377@nyu.edu>
2018-03-16 15:42:59 -1000

    Includes my resume, which I copy pasta'd from my nicolesandry github repo.

---
+++ client/src/components/Resume.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+
+	render(){
+		return(
+			<div id="Resume">
+				<h1 id="nicole-sandry">Nicole Sandry</h1>
+				<ol>
+					<li>(808) 392-1808</li>
+					<li><a href="mailto:nsandry@gmail.com" target="_blank" rel="noopener noreferrer">nsandry@gmail.com</a></li>
+					<li><a href="https://github.com/nms377" target="_blank" rel="noopener noreferrer">https://github.com/nms377</a></li>
+					<li><a href="https://www.linkedin.com/in/nicolesandry" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/nicolesandry</a></li>
+				</ol>
+				<h2 id="development-experience">Development Experience</h2>
+				<h3 id="hele">Front End Engineer</h3>
+				<h4 id="-hele-io-https-hele-io-"><a href="https://hele.io/" target="_blank" rel="noopener noreferrer">Hele.io</a></h4>
+				<h5 id="may-2017">Honolulu, HI May 2017</h5>
+				<ul>
+					<li>Created a web-based application generating a personal travel itinerary by the most affordable price</li>
+					<li>Created backend database with PostgreSQL and Sequelize ORM</li>
+					<li>Designed the User Interface with React, SASS, and Gulp</li>
+					</ul>
+				<h2 id="professional-experience">Professional Experience</h2>
+				<h3 id="executive-assistant">Office Manager & Social Media Manager</h3>
+				<h4 id="handcrafted-restaurants-llc"><a href="http://www.handcraftedrestaurants.com/" target="_blank" rel="noopener noreferrer">Handcrafted Restaurants</a></h4>
+				<h5 id="hi-aug-2013-present">Kailua, HI Aug 2013 – Dec 2017</h5>
+				<ul>
+					<li>Coordinated with third party developers and designers to maintain company websites and storefronts</li>
+					<li>Managed online reputation & marketing while building a following of 5,000+ on Instagram and 12,000+ on Facebook</li>
+					<li>Designed print collateral, menus, magazine ads, banners, posters, signage, and photography</li>
+					<li>Established menu editing protocols to maintain brand and product consistency across three restaurant concepts</li>
+					<li>Aided in the launch of two restaurant concepts and two restaurant acquisitions</li>
+				</ul>
+				<h2 id="education">Education</h2>
+				<h3 id="devleague">Computer Software Engineering</h3>
+				<h4 id="devleague">DevLeague, Nov 2016 - May 2017</h4>
+				<h4 id="completed-800-hours-learning-modern-web-development-languages-and-best-practices-for-building-data-driven-applications-contributed-to-a-team-focused-project-oriented-learning-environment-to-build-an-online-portfolio-">Completed 800+ hours learning modern web development languages and best practices for building data driven applications. Contributed to a team focused, project oriented learning environment to build an online portfolio.</h4>
+				<h3 id="bachelor-of-arts-in-interdisciplinary-studies">Bachelor of Arts in Interdisciplinary Studies</h3>
+				<h4 id="communications-with-a-minor-in-social-entrepreneurship">Communications, Minor in Social Entrepreneurship</h4>
+				<h5 id="new-york-university-gallatin-school-of-individualized-study-2011-2013">New York University - Gallatin School of Individualized Study, 2011 – 2013</h5>
+				<h2 id="technical-skills">Technical Skills</h2>
+				<div id="tech-skills-col">
+					<div class="skills">
+						<h3 id="languages">Languages</h3>
+						<ul>
+							<li>JavaScript</li>
+							<li>HTML5</li>
+							<li>CSS3</li>
+							<li>SQL</li>
+						</ul>
+					</div>
+					<div class="skills">
+						<h3 id="web-skills">Web Skills</h3>
+						<ul>
+							<li>Responsive/Mobile First Web Development</li>
+							<li>TDD/BDD</li>
+							<li>RDBMS/NoSQL</li>
+							<li>User Auth</li>
+							<li>API Integration</li>
+						</ul>
+					</div>
+					<div class="skills">
+						<h3 id="frontend-proficiencies">Frontend Proficiencies</h3>
+						<ul>
+							<li>ES6</li>
+							<li>React</li>
+							<li>Redux</li>
+							<li>Angular</li>
+							<li>SASS</li>
+						</ul>
+					</div>
+					<div class="skills">
+						<h3 id="server-side-proficiencies">Server-Side Proficiencies</h3>
+						<ul>
+							<li>NodeJS</li>
+							<li>Express</li>
+							<li>Sequelize ORM</li>
+						</ul>
+					</div>
+					<div class="skills">
+						<h3 id="database-proficiencies">Database Proficiencies</h3>
+						<ul>
+							<li>PostgreSQL </li>
+						</ul>
+					</div>
+					<div class="skills">
+						<h3 id="workflow-testing-utilities">Workflow/Testing Utilities</h3>
+						<ul>
+							<li>Git</li>
+							<li>Gulp</li>
+							<li>Mocha</li>
+							<li>Chai</li>
+							<li>SuperTest</li>
+						</ul>
+					</div>
+					<div class="skills">
+						<h3 id="business-development">Business Development</h3>
+						<ul>
+							<li>Adobe InDesign CC</li>
+							<li>Adobe Photoshop CC</li>
+							<li>Adobe Illustrator CC</li>
+						</ul>
+					</div>
+				</div>
+
+				<div id="tech-skills-col-print">
+					<div class="skills">
+						<ul>
+							<li>JavaScript</li>
+							<li>HTML5</li>
+							<li>CSS3</li>
+							<li>SQL</li>
+							<li>Responsive/Mobile First Web Development</li>
+							<li>TDD/BDD</li>
+							<li>RDBMS/NoSQL</li>
+							<li>User Auth</li>
+							<li>API Integration</li>
+							<li>ES6</li>
+							<li>React</li>
+							<li>Redux</li>
+							<li>Angular</li>
+							<li>SASS</li>
+							<li>NodeJS</li>
+							<li>Express</li>
+							<li>Sequelize ORM</li>
+							<li>PostgreSQL </li>
+							<li>Git</li>
+							<li>Gulp</li>
+							<li>Mocha</li>
+							<li>Chai</li>
+							<li>SuperTest</li>
+							<li>Adobe InDesign CC</li>
+							<li>Adobe Photoshop CC</li>
+							<li>Adobe Illustrator CC</li>
+					</ul>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default Resume;
