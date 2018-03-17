61b4b6880f8c9eaed2b274b33042a2159509eade
Nicole Sandry <nms377@nyu.edu>
2018-03-16 15:38:08 -1000

    Removed the link to my resume component and updated my about me description.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,9 +7,7 @@ class About extends Component {
 		return(
 			<div id="About">
 				<img src="/people/_RI_5176.jpg" alt="Self-portrait" height="333"></img>
-				<p>I am a photographer and web developer based on Oahu, Hawaii.</p>
-				
-				<p>I specialize in portraiture, food photography, and web design. <Link to='/contact'>Contact</Link> me for more information.</p>
+				<p>I am a Web Developer based on Oahu, Hawaii.</p>				
 			</div>
 		)
 	}
