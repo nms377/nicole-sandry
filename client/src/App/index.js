f92a77a22dfb8bb0b209c396d1403667638987da
Nicole Sandry <nms377@nyu.edu>
2018-03-18 13:26:33 -1000

    Removed the h2 element and imported the Work component.

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+import Work from '../components/Work.js';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h2>Hello World</h2>
-      </div>
+    	<Work />
     );
   }
 }
