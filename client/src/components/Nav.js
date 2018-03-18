8ba7a7ca814dfb253d8b22c109ba198998109e99
Nicole Sandry <nms377@nyu.edu>
2018-03-18 13:29:21 -1000

    Removed h1 element from root link.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,7 +5,7 @@ class Nav extends Component {
 	render() {
 		return (
 			<div id="Nav">
-				<Link to='/' id="Nicole-Sandry"><h1>Nicole Sandry</h1></Link>
+				<Link to='/' id="Nicole-Sandry">Nicole Sandry</Link>
 				<Link className="Nav-Link" to='/work'>Work</Link>
 				<Link className="Nav-Link" to='/resume'>Resume</Link>
 				<Link className="Nav-Link" to='/about'>About</Link>
