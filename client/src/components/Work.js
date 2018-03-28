e40a2f98a1f79cc0aa85902f7ae3607c08046f80
Nicole Sandry <nms377@nyu.edu>
2018-03-27 20:42:13 -1000

    Added content and edited the styling for work component. Included four dev projects that I feel best represent my experience so far.

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -2,39 +2,68 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 const HeleIo = require('../assets/heleio.png');
 const Leiover = require('../assets/leiover.png');
+const Kanban = require('../assets/react-kanban.png');
+const Instafeed = require('../assets/react-insta.png');
 
 
 class Work extends Component {
 
 	render() {
 		return(
-			<div id="work-container">
-				<h1>Work</h1>
-				<div className="Work">
-					<a href="https://hele.io/" target="_blank" rel="noopener noreferrer"><img src={ HeleIo } alt="Hele.io homepage"/></a>
-					<p>Hele</p>
-					<p>Developer</p>
-					<p>Find travel accommodations at an affordable price and consolidate them in a personal travel itinerary.</p>
-					<p>Collaborated with three other students as part of our capstone project at DevLeague.</p>
-					<ul>My Contributions:
-						<li>Creating the database schema using Sequelize ORM to collect user information and data from travel APIs</li>
-						<li>Designing the user interface with ReactJS, SASS, and CSS3</li>
-					</ul>
+			<div id="workContainer">
+				<div className="work">
+					<a href="https://hele.io/" target="_blank" rel="noopener noreferrer"><img className="workImg" src={ HeleIo } alt="Hele.io homepage"/></a>
+					<h1 className="company">Hele</h1>
+					<h4 className="position">Developer</h4>
+					<p className="description">Find travel accommodations at an affordable price and consolidate them in a personal travel itinerary.</p>
+					<p className="description">Collaborated with three other students as part of our capstone project at DevLeague.</p>
+					<div className="workContributions">
+						<h4 className="contributions">Contributions:</h4>
+						<ul>
+							<li>Creating the database schema using Sequelize ORM to collect user information and data from travel APIs</li>
+							<li>Designing the user interface with ReactJS, SASS, and CSS3</li>
+						</ul>
+					</div>
 				</div>
-				<div className="Work">
-					<a href="https://github.com/nms377/ATTHackathon-luggage" target="_blank" rel="noopener noreferrer"><img src={ Leiover } alt="Luggage Lei Over"/></a>
-					<p>Luggage Lei Over Hackathon Project</p>
-					<p>Developer</p>
-					<p>A potential service transporting luggage from the airport to hotel allowing visitors to explore without worrying about where to store their belongings.</p>
-					<p>Created during AT&T IoT Hackathon March 2017 with team mates Danika and Laylo.</p>
-					<p>Created the database schema using Sequelize ORM to store data from our forms built using Handlebars. Using a Bootstrap theme allowed us to focus more on back end development during the two day hackathon.</p>
-					<p>Code Repository: <a href="https://github.com/nms377/ATTHackathon-luggage" target="_blank" rel="noopener noreferrer">https://github.com/nms377/ATTHackathon-luggage</a></p>
+				<div className="work">
+					<a href="https://github.com/nms377/ATTHackathon-luggage" target="_blank" rel="noopener noreferrer"><img className="workImg" src={ Leiover } alt="Luggage Lei Over"/></a>
+					<h1 className="company">Luggage Lei Over Hackathon Project</h1>
+					<h4 className="position">Developer</h4>
+					<p className="description">A potential service transporting luggage from the airport to hotel allowing visitors to explore without worrying about where to store their belongings.</p>
+					<p className="description">Created during AT&T IoT Hackathon March 2017 with two other developers.</p>
+					<div className="workContributions">
+						<h4 className="contributions">Contributions:</h4>
+						<ul>
+							<li>Created the database schema using Sequelize ORM and Postgresql to store reservations and user information.</li>
+							<li>Used Handlebars to design reservation forms and confirmation pages.</li>
+						</ul>
+					</div>
 				</div>
-				<div className="Work">
-					<a>Screenshot</a>
-					<p>Company</p>
-					<p>Position</p>
-					<p>Description</p>
+				<div className="work">
+					<a href="https://github.com/nms377/React-Kanban" target="_blank" rel="noopener noreferrer"><img className="workImg" src={ Kanban } alt="React Kanban" /></a>
+					<h1 className="company">React Kanban</h1>
+					<p className="description">A digital Kanban board made with React. A portfolio project created in DevLeague's Full Stack JavaScript development bootcamp.</p>
+					<div className="workContributions">
+						<h4 className="contributions">Built Using:</h4>
+						<ul>
+							<li>Front-end made with ReactJS</li>
+							<li>HTML, CSS, & SASS for Responsive/Mobile First Web Development</li>
+							<li>Back end server made with Express</li>
+							<li>Database designed with Sequelize ORM and Postgresql</li>
+						</ul>
+					</div>
+				</div>
+				<div className="work">
+					<a href="https://nms377.github.io/react-instafeed/" target="_blank" rel="noopener noreferrer"><img className="workImg" src={ Instafeed } alt="Recreationg of my Instagram feed using React" /></a>
+					<h1 className="company">React Instafeed</h1>
+					<p className="description">Recreation of my personal Instagram feed using React.</p>
+					<div className="workContributions">
+						<h4 className="contributions">Built Using:</h4>
+						<ul>
+							<li>Front-end made with ReactJS</li>
+							<li>HTML, CSS, & SASS for Responsive/Mobile First Web Development</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		)
