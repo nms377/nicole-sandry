4375892a8a69a637ba9a5a724792e4c18fa58b8a
Nicole Sandry <nms377@nyu.edu>
2018-03-29 10:22:36 -1000

    Created two nav elements and setup a function to hide and display one of the navs if the mobile menu icon is clicked. I hid the desktop nav for now. I will update when I begin styling for desktop.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import List from '../assets/list.svg';
 
 class Nav extends Component {
+	constructor(props){
+		super(props);
+			this.state = {
+				showMobileMenu: false
+			};
+	}
+
+	// Toggle to show and hide mobile menu
+	operation(){
+		this.setState({
+			showMobileMenu: !this.state.showMobileMenu
+		});
+	}
 	render() {
 		return (
-			<div id="Nav">
-				<Link to='/' id="nicoleSandry">Nicole Sandry</Link>
+			<div id="nav-container">
+				<div id="Nav">
+					<Link to='/' id="nicoleSandry">Nicole Sandry</Link>
 					<Link className="NavLink" to='/work'>Work</Link>
 					<Link className="NavLink" to='/resume'>Resume</Link>
 					<Link className="NavLink" to='/about'>About</Link>
+					<div id="mobile-icon" onClick={() => this.operation()}>
+						<img src={List} alt="Mobile menu list icon" />
+					</div>
+				</div>
+				{
+					this.state.showMobileMenu?
+					<div id="mobile-nav">
+						<Link className="NavLinkMobile" to='/work'>Work</Link>
+						<Link className="NavLinkMobile" to='/resume'>Resume</Link>
+						<Link className="NavLinkMobile" to='/about'>About</Link>
+					</div>
+					:null
+				}
 			</div>
 		)
 	}
