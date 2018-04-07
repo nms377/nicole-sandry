8b1c3749d03f80bc96404e851c80f7bdec9c204f
Nicole Sandry <nms377@nyu.edu>
2018-04-06 19:21:23 -1000

    Working on the About page.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Portrait from '../assets/_RI_5176.jpg';
 
 class About extends Component {
 
 	render(){
 		return(
 			<div id="About">
-				<img src="/people/_RI_5176.jpg" alt="Self-portrait" height="333"></img>
-				<p>I am a Web Developer based on Oahu, Hawaii.</p>				
+				<div id="About-Container">
+					<img src={Portrait} alt="Self-portrait" height="333"></img>
+					<div id="text">
+						<p>I am a Web Developer based on Oahu, Hawaii.</p>				
+						<p>I am a Web Developer based on Oahu, Hawaii.</p>				
+					</div>
+				</div>
 			</div>
 		)
 	}
