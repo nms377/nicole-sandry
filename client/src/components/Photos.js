0368ac268e0dbdcb965ecf941ea8b543fcedfcf3
Nicole Sandry <nms377@nyu.edu>
2018-04-10 23:24:49 -1000

    Experimented with the code to see what the route code could do.

--- client/src/components/Photos.js
+++ client/src/components/Photos.js
@@ -3,15 +3,8 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 const Photos = () => (
 	<div>
-		<ul>
-			<li>
 				<NavLink to="/work/photos/people" activeStyle={{ textDecoration: 'underline' }}>People</NavLink>
-			</li>
-			<li>
 				<NavLink to="/work/photos/food" activeStyle={{ textDecoration: 'underline' }}>Food</NavLink>
-			</li>
-		</ul>
-
 		{ routes.map((route,i) => <RouteWithSubRoutes key={i} {...route} />)}
 	</div>
 );
