ff38e1460ec0b7f2dc17c4f0d784714db01016b1
Nicole Sandry <nms377@nyu.edu>
2018-04-10 23:33:04 -1000

    Looked up react router at reactraining.com to see how I could render a sub nav  that allowed me to include a text on the Work page in addition to the sub nav.

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,66 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-import Photos from './Photos';
-import Developer from './Developer';
+const Work = ({ match }) => (
+  <div>
+    <ul>
+      <li>
+        <NavLink to={`${match.url}/developer`}  activeStyle={{ textDecoration: 'underline', fontWeight: '600' }}>Web Design</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/photos`}  activeStyle={{ textDecoration: 'underline', fontWeight: '600' }}>Photos</NavLink>
+      </li>
+    </ul>
 
-class Work extends Component{
-	constructor(props){
-		super(props);
-			this.state = {
-				hideDeveloperComponent: true
-			};
-	}
-
-	// Toggle to show and hide mobile menu
-	hideDev(){
-		this.setState({
-			hideDeveloperComponent: false
-		});
-	}
-
-	render () {
-		return (
-			<div>
-				<ul>
-					<li>
-						<NavLink to="/work/developer" activeStyle={{ textDecoration: 'underline' }}>Web Design</NavLink>
-					</li>
-					<li>
-						<NavLink to="/work/photos" activeStyle={{ textDecoration: 'underline' }} onClick={() => this.hideDev()}>Photography</NavLink>
-					</li>
-				</ul>
-			
-			{
-				this.state.hideDeveloperComponent?
-					<Developer />
-				:null
-			}
-
-				{ routes.map((route,i) => <RouteWithSubRoutes key={i} {...route} />)}
-			</div>
-		)
-	}
-}
-
-const routes = [
-	{
-		path: "/work/developer",
-		component: Developer
-	},
-	{
-		path: "/work/photos",
-		component: Photos
-	}
-];
-
-const RouteWithSubRoutes = route => (
-	<Route
-		path={route.path}
-		render={props => (
-			<route.component {...props} routes={route.routes} />
-		)}
-	/>
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
 );
 
 export default Work;
