de33e01c2f735a4761e80d68def11ac56f6cbb0b
Nicole Sandry <nms377@nyu.edu>
2018-04-10 15:09:50 -1000

    This is the component I added a developer and photos components. Not the App/index.js component.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import About from './components/About';
 import Work from './components/Work';
 import Resume from './components/Resume';
+import Developer from './components/Developer';
+import Photos from './components/Photos';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import './index.css';
@@ -16,10 +18,11 @@ ReactDOM.render(
 	<Router>
 		<div>
 			<Nav />
-			<Route exact path='/' component={ Work } />
+			<Route exact path='/' component={ Developer } />
 			<Route path='/work' component={ Work } />
 			<Route path='/resume' component={ Resume } />
 			<Route path='/about' component={ About } />
+			<Route path='/work/photos' component={ Photos } />
 		  <Footer />
 	  </div>
   </Router>,
