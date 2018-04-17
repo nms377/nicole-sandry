d89b0e709bf4b8001069ff463f3d69f5e0eef833
Nicole Sandry <nms377@nyu.edu>
2018-04-17 10:24:58 -1000

    Removed the sub nav for photography and kept the Developer component. I will continue working on the subnav and deploy the updated site once it is ready.

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,24 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-
+import React, { Component } from 'react'; 
 import Developer from './Developer';
 
 const Work = ({ match }) => (
   <div>
-    <ul>
-      <li>
-        <NavLink to={`${match.url}/developer`}  activeStyle={{ textDecoration: 'underline', fontWeight: '600' }}>Web Design</NavLink>
-      </li>
-      <li>
-        <NavLink to={`${match.url}/photos`}  activeStyle={{ textDecoration: 'underline', fontWeight: '600' }}>Photos</NavLink>
-      </li>
-    </ul>
-
-    <Route
-      exact
-      path={match.url}
-      render={() => <Developer />}
-    />
+    <Developer />
   </div>
 );
 
