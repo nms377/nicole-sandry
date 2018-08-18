e637cf06aa747263c0f48e913fe3a60e4174bce2
Nicole Sandry <nms377@nyu.edu>
2018-08-18 01:11:07 -1000

    Removed the old code I worked on. Instead of separting my web development work from my photography work I decided to consolidate them all on a page together. They will be in a card format using CSS grid. This gives the visitor a brief description of each project and if a visitor clicks on a card it will redirect them to a page focusing on that project.

--- client/src/components/Work-2.js
+++ client/src/components/Work-2.js
@@ -3,27 +3,70 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import Developer from "./Developer";
 
-const Work = ({ match }) => (
-  <div>
-    <ul>
-      <li>
-        <NavLink
-          to={`${match.url}/developer`}
-          activeStyle={{ textDecoration: "underline", fontWeight: "600" }}
-        >
-          Web Design
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`${match.url}/photos`}
-          activeStyle={{ textDecoration: "underline", fontWeight: "600" }}
-        >
-          Photos
-        </NavLink>
-      </li>
-    </ul>
-  </div>
-);
+const workIndex = [
+  {
+    title: "Hele",
+    description: "A web app that finds travel accommodations and builds a custom travel itinerary."
+  },
+  {
+    title: "AT&T Hackathon 2017",
+    description: "A hypothetical luggage concierge servicing Oahu visitors."
+  },
+  {
+    title: 'React Kanban',
+    description: 'A digital Kanban board made with React.'
+  },
+  {
+    title: 'React Instafeed',
+    description: 'Recreation of my personal Instagram feed using ReactJS.'
+  },
+  {
+    title: 'Monkeypod Kitchen',
+    description: 'Food Photography.'
+  },
+  {
+    title: 'Moku Kitchen',
+    description: 'Food Photography.'
+  },
+  {
+    title: 'The Beach House Restaurant',
+    description: 'Food Photography.'
+  },
+  {
+    title: 'Portraits',
+    description: 'Grad Photos, Engagements, and Maternity Shoots.'
+  }
+];
+
+class Work extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mouseHoverShowDescription: false
+    };
+  }
+
+  // Mouse over element to show work description
+  mouseHoverShowDescription() {
+    this.setState({
+      mouseHoverShowDescription: !this.state.mouseHoverShowDescription
+    });
+  }
+
+  render() {
+    return (
+      <div id="workMainContainer">    
+        <div className="WorkComponents">
+          {workIndex.map((el, i, arr) => (
+            <div className="WorkContainers" key={i}>
+              <h2 className="WorkTitle">{el.title}</h2>
+              <p className="WorkDescription">{el.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Work;
